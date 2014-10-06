680a8ba1b00b45ad6f76a76374f2b83cfb3c56df
Peter <peter.graf@nrel.gov>
2014-10-06 17:50:07 -0600

    windows compile args

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os
 
 if os.name == 'nt':  # Windows.
 #    extra_compile_args = ['/TC', '/D', 'ANSI'] # for msvs (we use mingw (gcc) now by default)
-    pass
+    extra_compile_args = ''
 else:
     extra_compile_args = ''
 
