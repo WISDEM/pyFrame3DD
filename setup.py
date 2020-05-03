f0ef371c46f4c573883cd248d65568d4c149781c
Garrett Barter <garrett.barter@nrel.gov>
2020-05-02 22:04:49 -0600

    cleaning up directory structure and makingn compatible with PEP8

--- setup.py
+++ setup.py
@@ -1,33 +1,43 @@
 # setup.py
 # only if building in place: ``python setup.py build_ext --inplace``
 
-from setuptools import setup
-from distutils.core import setup, Extension
 import os
-# from numpy.distutils.core import setup, Extension
+import sys
+import platform
+import glob
+from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
 
-
+os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 
 #if os.name == 'nt':  # Windows.
 #    extra_compile_args = ['/TC', '/D', 'ANSI'] # for msvs
 #     # TODO: Not with Anaconda MINGW
 #else:
-extra_compile_args = ''
+#extra_compile_args = ''
 
+froot = 'pyframe3dd' + os.sep + 'src' + os.sep
+pyframeExt = Extension('pyframe3dd._pyframe3dd', sources=[froot+'py_HPGmatrix.c',
+                                               froot+'HPGutil.c',
+                                               froot+'NRutil.c',
+                                               froot+'coordtrans.c',
+                                               froot+'preframe.c',
+                                               froot+'py_eig.c',
+                                               froot+'py_frame3dd.c',
+                                               froot+'py_io.c',
+                                               froot+'py_main.c'])
 
 setup(
     name='pyFrame3DD',
-    version='1.1.0',
+    version='1.1.1',
     description='Python bindings to Frame3DD',
     author='NREL WISDEM Team',
     author_email='systems.engineering@nrel.gov',
-    package_dir={'': 'src'},
-    py_modules=['frame3dd'],
+    #package_dir={'': 'src'},
+    #py_modules=['pyframe3dd'],
     package_data={'pyframe3dd': []},
     packages=['pyframe3dd'],
     license='Apache License, Version 2.0',
-    ext_modules=[Extension('_pyframe3dd', ['src/pyframe3dd/py_main.c', 'src/pyframe3dd/py_io.c',
-                                           'src/pyframe3dd/py_frame3dd.c', 'src/pyframe3dd/py_HPGmatrix.c', 'src/pyframe3dd/coordtrans.c',
-                                           'src/pyframe3dd/py_eig.c', 'src/pyframe3dd/HPGutil.c', 'src/pyframe3dd/NRutil.c'],
-                 extra_compile_args=extra_compile_args)]
+    ext_modules=[pyframeExt],
+    zip_safe=False
 )
--- src/pyframe3dd/__init__.py
+++
@@ -1,4 +0,0 @@
-from .frame3dd import Frame, StaticLoadCase, NodeData, ReactionData, ElementData, Options
-#import frame3dd
-
-    
