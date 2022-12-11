6c54758a3964734e0d495ff84bf3bac6b8f9b5b2
Garrett Barter <garrett.barter@nrel.gov>
2022-12-11 05:29:54 -0700

    working scikit-build for c-extension

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ import os
 import sys
 import platform
 import glob
-from setuptools import setup, find_packages
-from numpy.distutils.core import setup, Extension
+from setuptools import find_packages
+from skbuild import setup
 
-os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
+#os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 
 #if os.name == 'nt':  # Windows.
 #    extra_compile_args = ['/TC', '/D', 'ANSI'] # for msvs
@@ -16,16 +16,16 @@ os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
 #else:
 #extra_compile_args = ''
 
-froot = 'pyframe3dd' + os.sep + 'src' + os.sep
-pyframeExt = Extension('pyframe3dd._pyframe3dd', sources=[froot+'py_HPGmatrix.c',
-                                               froot+'HPGutil.c',
-                                               froot+'NRutil.c',
-                                               froot+'coordtrans.c',
-                                               froot+'preframe.c',
-                                               froot+'py_eig.c',
-                                               froot+'py_frame3dd.c',
-                                               froot+'py_io.c',
-                                               froot+'py_main.c'])
+#froot = 'pyframe3dd' + os.sep + 'src' + os.sep
+#pyframeExt = Extension('pyframe3dd._pyframe3dd', sources=[froot+'py_HPGmatrix.c',
+#                                               froot+'HPGutil.c',
+#                                               froot+'NRutil.c',
+#                                               froot+'coordtrans.c',
+#                                               froot+'preframe.c',
+#                                               froot+'py_eig.c',
+#                                               froot+'py_frame3dd.c',
+#                                               froot+'py_io.c',
+#                                               froot+'py_main.c'])
 
 setup(
     name='pyFrame3DD',
@@ -35,9 +35,11 @@ setup(
     author_email='systems.engineering@nrel.gov',
     #package_dir={'': 'src'},
     #py_modules=['pyframe3dd'],
-    package_data={'pyframe3dd': []},
+    #package_data={'pyframe3dd': []},
     packages=['pyframe3dd'],
     license='Apache License, Version 2.0',
-    ext_modules=[pyframeExt],
-    zip_safe=False
+#    ext_modules=[pyframeExt],
+    zip_safe=False,
+    #cmake_install_dir='pyframe3dd',
+    #cmake_source_dir='pyframe3dd' + os.sep + 'src' + os.sep,
 )
